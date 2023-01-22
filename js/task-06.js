11d9e755b5959470859150a8ff61744ca9b21982
Oleksandr Yushchenko <sanyeks@gmail.com>
2023-01-22 19:08:50 +0200

    fix 6 task from the mentor's comments

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,12 @@
 const input = document.querySelector('#validation-input');
 input.addEventListener('blur', onInputBlur);
 function onInputBlur(event) {
-    if (event.currentTarget.value.length < input.getAttribute('data-length')) {
-        input.classList.add('invalid');
-    }
-    else if (event.currentTarget.value.length >= input.getAttribute('data-length')) {
-        input.classList.replace('invalid', 'valid');
+    if (event.currentTarget.value.length == input.dataset.length) {
+        input.classList.add("valid");
+        input.classList.remove("invalid");
+    } 
+    else {
+        input.classList.add("invalid");
+        input.classList.remove("valid");
     }
 }
